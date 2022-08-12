f5e6b3a7a1f9cf5bfe177fac1b1235ad0cbcb60c
alexghz81 <alex-ghz@yandex.ru>
2022-08-12 19:19:06 +0300

    add forgot password page

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,10 +1,49 @@
 import React from "react";
+import styles from "./form.module.css";
+import {
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
+import { fetchRegister, formSetValue } from "../services/reducers/auth-slice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ForgotPassword = () => {
+  const dispatch = useDispatch();
+  const { email } = useSelector((state) => state.auth.form);
+
+  const onChange = (evt) => {
+    dispatch(formSetValue({ input: evt.target.name, value: evt.target.value }));
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    // dispatch(fetchRegister({ name, email, password }));
+  };
+
   return (
-    <>
-      <h1>ForgotPassword PAGE</h1>
-    </>
+    <div className={styles.wrapper}>
+      <h1 className="text_type_main-medium mt-10 pt-10 mb-6">
+        Восстановление пароля
+      </h1>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={`${styles.input_wrapper} mb-5`}>
+          <Input
+            onChange={onChange}
+            value={email}
+            name="email"
+            placeholder="Укажите e-mail"
+          />
+        </div>
+        <Button primary>Восстановить</Button>
+      </form>
+      <p className="text_type_main-default text_color_inactive pt-10 mt-10">
+        Вспомнили пароль?&nbsp;
+        <Link to="/login" className={styles.link}>
+          Войти
+        </Link>
+      </p>
+    </div>
   );
 };
 
