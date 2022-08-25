1421ac9b16eb2b71bb895f14931842f1f6b55aa1
alexghz81 <alex-ghz@yandex.ru>
2022-08-25 19:50:58 +0300

    component from order list

--- src/components/orders-list/orders-list.jsx
+++ src/components/orders-list/orders-list.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styles from "../../pages/feed/feed.module.css";
+import { Link, useLocation, useRouteMatch } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+import OrdersItem from "../orders-item/orders-item";
 
-const OrdersList = () => {
-  return <div></div>;
+const OrdersList = ({ orders }) => {
+  const match = useRouteMatch();
+  const location = useLocation();
+  return (
+    <div className={styles.orders_feed}>
+      <div>
+        <h1 className="text text_type_main-large pb-5">Лента заказов</h1>
+      </div>
+      <div className={`${styles.orders_wrapper} pr-2`}>
+        {orders.map((order) => (
+          <Link
+            className={styles.link}
+            key={uuid()}
+            to={{
+              pathname: `${match.url}/${order._id}`,
+              state: { background: location, data: order },
+            }}
+          >
+            <OrdersItem order={order} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersList;
--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -6,16 +6,12 @@ import {
   wsConnectionStart,
 } from "../../services/reducers/ws-slice";
 import { ORDERS_URL } from "../../utils/constants";
-import OrdersItem from "../../components/orders-item/orders-item";
-import { Link, useLocation, useRouteMatch } from "react-router-dom";
-import { v4 as uuid } from "uuid";
+import OrdersList from "../../components/orders-list/orders-list";
 
 const Feed = () => {
   const { wsConnected, wsMessages } = useSelector((state) => state.ws);
   const dispatch = useDispatch();
   const { orders = [], total, totalToday } = wsMessages;
-  const match = useRouteMatch();
-  const location = useLocation();
 
   useEffect(() => {
     if (!wsConnected) {
@@ -31,26 +27,56 @@ const Feed = () => {
   return (
     orders.length > 0 && (
       <section className={styles.wrapper}>
-        <div className={styles.orders_feed}>
-          <div>
-            <h1 className="text text_type_main-large pb-5">Лента заказов</h1>
-          </div>
-          <div className={`${styles.orders_wrapper} pr-2`}>
-            {orders.map((order) => (
-              <Link
-                className={styles.link}
-                key={uuid()}
-                to={{
-                  pathname: `${match.url}/${order._id}`,
-                  state: { background: location, data: order },
-                }}
+        <OrdersList orders={orders} />
+        <div className={styles.orders_status}>
+          <div className={styles.status_wrapper}>
+            <div className={styles.completed_orders}>
+              <div
+                className={`${styles.order_title} text text_type_main-medium`}
               >
-                <OrdersItem order={order} />
-              </Link>
-            ))}
+                Готовы:
+              </div>
+              <div
+                className={`${styles.completed_orders_numbers} text text_type_digits-default mb-2`}
+              >
+                12345
+              </div>
+            </div>
+            <div className={styles.in_progress_orders}>
+              <div
+                className={`${styles.order_title} text text_type_main-medium`}
+              >
+                В работе:
+              </div>
+              <div
+                className={`${styles.orders_numbers} text text_type_digits-default mb-2`}
+              >
+                5678
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.all_time_orders} pt-15 pb-15`}>
+            <span className="text text_type_main-medium">
+              Выполнено за все время:
+            </span>
+            <h3
+              className={`${styles.all_time_orders_number} text text_type_digits-large`}
+            >
+              {total && total.toString().substring(0, 2)}&nbsp;
+              {total && total.toString().substring(2)}
+            </h3>
+          </div>
+          <div className={styles.today_orders}>
+            <span className="text text_type_main-medium">
+              Выполнено за сегодня:
+            </span>
+            <h3
+              className={`${styles.today_orders_number} text text_type_digits-large`}
+            >
+              {totalToday}
+            </h3>
           </div>
         </div>
-        <div className={styles.orders_status}>STATUS</div>
       </section>
     )
   );
